7550f9ec116c8d358bceb483affb875f50054b88
Dinesh Magar <magard1010@gmail.com>
2024-03-07 00:03:49 +0545

    staff page request list detail CRUD

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.index, name="dashboard_index"),
     path("staff/", views.staff, name="dashboard_staff"),
+    path("staff/detail/<int:pk>/", views.staff_detail, name="dashboard_staff_detail"),
     path("product/", views.product, name="dashboard_product"),
+    path("product/delete/<int:pk>/", views.product_delete, name="dashboard_product_delete"),
+    path("product/update/<int:pk>/", views.product_update, name="dashboard_product_update"),
     path("order/", views.order, name="dashboard_order"),
     
 ]
--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,25 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from django.contrib.auth.decorators import login_required
 
-from .models import Product
+from .models import Product, Order
+
+from .forms import ProductForm, OrderForm
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
+
 @login_required
 def index(request):
-    return render(request, "dashboard/index.html")
+    orders = Order.objects.all()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.staff = request.user
+            instance.save()
+            return redirect("dashboard_index")
+    else:
+        form = OrderForm()
+
+    context = {
+        "orders": orders,
+        "form": form,
+    }
+    return render(request, "dashboard/index.html", context)
+
 
 @login_required
 def staff(request):
-    return render(request, "dashboard/staff.html")
+    workers = User.objects.all()
+    context = {"workers": workers}
+    return render(request, "dashboard/staff.html", context)
+
+
+@login_required
+def staff_detail(request, pk):
+    workers = User.objects.get(id=pk)
+    context = {"workers": workers}
+    return render(request, "dashboard/staff_detail.html", context)
+
 
 @login_required
 def product(request):
-    return render(request, "dashboard/product.html")
+    items = Product.objects.all()  # using ORM
+    # items = Product.objects.raw('SELECT * FROM inventory_app_product') # using sql query
+
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard_product")
+    else:
+        form = ProductForm()
+
+    context = {
+        "items": items,
+        "form": form,
+    }
+    return render(request, "dashboard/product.html", context)
+
+
+@login_required
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == "POST":
+        item.delete()
+        return redirect("dashboard_product")
+    return render(request, "dashboard/product_delete.html")
+
+
+@login_required
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard_product")
+    else:
+        form = ProductForm(instance=item)
+
+    context = {"form": form}
+    return render(request, "dashboard/product_update.html", context)
+
 
 @login_required
 def order(request):
-    return render(request, "dashboard/order.html")
+    orders = Order.objects.all()
+    context = {"orders": orders}
+    return render(request, "dashboard/order.html", context)
